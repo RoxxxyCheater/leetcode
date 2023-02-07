f522c5ceb22cae3a68551ef9a4a6f05da82e050c
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2023-02-07 14:54:57 +0200

    [1] final refactor

--- IntegerToRoman[12].py
+++ IntegerToRoman[12].py
@@ -52,13 +52,19 @@
 # Acceptance Rate
 # 61.8%
 
-
-
+roman_dict,res = {1: "I",4: "IV",5: "V",9: "IX",10: "X",40: "XL",50: "L",90: "XC",100: "C",400: "CD",500: "D",900: "CM",1000: "M"}, ""
 class Solution(object):
     def intToRoman(self, num):
         """
         :type num: int
         :rtype: str
         """
-        return num
-        
+        for n in sorted(list(roman_dict.keys()))[::-1]:
+            while num >= n:
+                num -= n
+                res += roman_dict[n]
+        return res
+
+list_nums = 3, 58, 1994,1993
+for i in list_nums:
+    Solution.intToRoman('Success', i)
