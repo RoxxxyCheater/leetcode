45a274d3532bfa59a44d164d1504c3612b275317
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2023-02-16 13:17:23 +0200

    [0] task

---
+++ MaximumDepthOfBinaryTree[104].py
@@ -0,0 +1,50 @@
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+# Example 1:
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+
+# Example 2:
+
+# Input: root = [1,null,2]
+# Output: 2
+
+ 
+
+# Constraints:
+
+#     The number of nodes in the tree is in the range [0, 104].
+#     -100 <= Node.val <= 100
+
+# Accepted
+# 2.2M
+# Submissions
+# 3M
+# Acceptance Rate
+# 73.6%
+# Definition for a binary tree node.
+#
+# 
+# 
+# 
+#  class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0 
+
+list_nums = [3,9,20,'null','null',15,7] , [1,'null',2]
+for i in list_nums:
+    Solution.maxDepth('Success', i)
