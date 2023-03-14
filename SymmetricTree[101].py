4bf7e18652419bbf019de1b5caa30ae820f5bbd2
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2023-03-14 22:43:38 +0200

    [1] func task

--- SymmetricTree[101].py
+++ SymmetricTree[101].py
@@ -27,3 +27,22 @@
 # 2.9M
 # Acceptance Rate
 # 54.2%
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return root
+
+roots_list = [1,2,2,3,4,4,3],[1,2,2,null,3,null,3]
+
+for i in roots_list:
+    Solution.isSymmetric('Success', i)
