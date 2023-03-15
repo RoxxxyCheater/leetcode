eff6e2345e60166a0f7890221040e99346c4c432
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-03-15 19:53:21 +0100

    [1] task func

--- CheckCompletenessОfaBinaryTree[958].py
+++ CheckCompletenessОfaBinaryTree[958].py
@@ -29,3 +29,20 @@
 # Acceptance Rate
 # 55.8%
 # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isCompleteTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return root
+    
+    
+roots =[1,2,3,4,5,null,7],[1,2,3,4,5,6]
+for i in roots:
+    Solution.isCompleteTree('Success', i)
