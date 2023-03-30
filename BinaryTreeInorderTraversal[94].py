7c39a63898249b9a5935b0fbaf8f59ea884e5ed2
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-03-30 23:16:27 +0200

    [1] func task

--- BinaryTreeInorderTraversal[94].py
+++ BinaryTreeInorderTraversal[94].py
@@ -30,3 +30,22 @@
 # 2.6M
 # Acceptance Rate
 # 73.8%
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        return root
+    
+list_root =  [1,null,2,3], [], [1]
+for i in list_root:
+   Solution.inorderTraversal('Success', i)
