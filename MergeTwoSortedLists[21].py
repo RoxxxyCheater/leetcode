faa9d7c3c2bf2e15e1478f3a34d49e0e2b3254f4
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-19 22:44:03 +0200

    [1] func

--- MergeTwoSortedLists[21].py
+++ MergeTwoSortedLists[21].py
@@ -3,3 +3,24 @@
 # Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 # Return the head of the merged linked list.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        return list1 + list2
+
+      
+list1 = [1,2,4],[],[]
+list2 = [1,3,4],[],[0]
+
+for index, num in enumerate(list1):
+  Solution.mergeTwoLists('Success', num, list2[index])
