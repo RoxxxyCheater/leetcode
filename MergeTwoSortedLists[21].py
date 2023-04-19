df69deccb2eff20f81da3fb9c84cdd0e57096a4c
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-19 22:44:42 +0200

    [2] final without explain

--- MergeTwoSortedLists[21].py
+++ MergeTwoSortedLists[21].py
@@ -16,7 +16,17 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        return list1 + list2
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
 
       
 list1 = [1,2,4],[],[]
