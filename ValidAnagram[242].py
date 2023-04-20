c03b5fd5956d561acf0f3623796dbacbb6aa7187
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-20 22:34:56 +0200

    [2] final without explain

--- ValidAnagram[242].py
+++ ValidAnagram[242].py
@@ -37,10 +37,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return s, t
+        if len(s) != len(t):
+            return False
+        s_dict = {}
+        t_dict = {}
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+        return s_dict == t_dict
        
 list_s = "anagram","rat"
 list_t = "nagaram","car"
  
 for index, num in enumerate(list_s):
-    Solution.addToArrayForm('Success', num, list_t[index])
+    Solution.isAnagram('Success', num, list_t[index])
