eacd8c8fe458ae4882e296e40351abfd3585dadf
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-29 00:51:09 +0300

    [3] final with explain

--- ValidPalindrome[125].py
+++ ValidPalindrome[125].py
@@ -37,14 +37,15 @@
 # 4.3M
 # Acceptance Rate
 # 44.5%
+
 class Solution(object):
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        s = ''.join(filter(str.isalnum, str(s))).lower()
-        return s == s[::-1]
+        s = ''.join(filter(str.isalnum, str(s))).lower() # Удаляем все не-алфанумерические символы из строки и приводим к нижнему регистру
+        return s == s[::-1] # Возвращаем полученную строку если является палиндромом
 
 list_s = "A man, a plan, a canal: Panama","race a car", " "
 for i in list_s:
