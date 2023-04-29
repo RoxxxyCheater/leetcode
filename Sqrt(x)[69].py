458ecb22accbb224fab536e7ae7086cd334527fc
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-30 00:46:24 +0300

    [2] final witjhout explain

--- Sqrt(x)[69].py
+++ Sqrt(x)[69].py
@@ -38,7 +38,17 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return x
+        if x == 0:
+            return 0
+        left, right = 1, x
+        while True:
+            mid = (left + right) // 2
+            if mid > x / mid:
+                right = mid - 1
+            else:
+                if (mid + 1) > x / (mid + 1):
+                    return mid
+                left = mid + 1
     
 list_x = 4,8
 for i in list_x:
