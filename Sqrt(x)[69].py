d6f85353ad695d4fe735b8c952f3a8f0bba3f430
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-04-30 00:48:30 +0300

    [3] final with explain

--- Sqrt(x)[69].py
+++ Sqrt(x)[69].py
@@ -38,17 +38,17 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x == 0:
-            return 0
-        left, right = 1, x
-        while True:
-            mid = (left + right) // 2
-            if mid > x / mid:
+        left, right = 0, x  # задаем начальные границы для бинарного поиска
+        while left <= right:  # запускаем бинарный поиск
+            mid = (left + right) // 2  # находим середину отрезка
+
+            if mid * mid <= x < (mid + 1) * (mid + 1):  # если нашли корень, возвращаем его
+                return mid
+            elif x < mid * mid:  # если корень больше середины, идем в левую часть
                 right = mid - 1
-            else:
-                if (mid + 1) > x / (mid + 1):
-                    return mid
+            else:  # если корень меньше середины, идем в правую часть
                 left = mid + 1
+        return -1  # если корень не найден, возвращаем -1
     
 list_x = 4,8
 for i in list_x:
