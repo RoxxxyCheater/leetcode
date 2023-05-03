878246c6a6988e6c162d7596dcbf35f3417370f3
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-03 23:50:05 +0300

    [2] final without explain

--- IsomorphicStrings[205].py
+++ IsomorphicStrings[205].py
@@ -42,6 +42,14 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        s_to_t = {}
+        t_to_s = {}
+        for i in range(len(s)):
+            if s[i] not in s_to_t and t[i] not in t_to_s:
+                s_to_t[s[i]] = t[i]
+                t_to_s[t[i]] = s[i]
+            elif s_to_t.get(s[i]) != t[i] or t_to_s.get(t[i]) != s[i]:
+                return False
         return True
 
 
