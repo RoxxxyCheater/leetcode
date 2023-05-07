662d3fc62578d2cecd774deb11db5264c6e8f27b
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-07 23:51:46 +0200

    [3] final with explain

--- FibonacciNumber[509].py
+++ FibonacciNumber[509].py
@@ -26,14 +26,25 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+
+    # отсекаем если n равно 0, возвращаем 0
+    if n == 0:
+        return 0
+    # отсекаем если n равно 1, возвращаем 1
+    elif n == 1:
+        return 1
+    # рекурсивно вызываем функцию fib для n-1 и n-2 и складываем результаты
+    else:
+        return fib(n-1) + fib(n-2)
 
 list_n = 2,3,4
 
 for i in list_n:
     Solution.fib("Success", i)
+
+
+
+
+
+
+
