330ac12901dcf3caae3b362f4746623184e1f9e7
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-11 23:20:59 +0200

    [3] final with explain

--- UncrossedLines[1035].py
+++ UncrossedLines[1035].py
@@ -44,16 +44,19 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(nums1), len(nums2)
-        dp = [[0] * (n+1) for _ in range(m+1)] 
+        dp = [[0] * (n+1) for _ in range(m+1)]  # Создаем матрицу dp с размером (m+1) x (n+1)
 
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if nums1[i-1] == nums2[j-1]: 
-                    dp[i][j] = dp[i-1][j-1] + 1
+                if nums1[i-1] == nums2[j-1]:  # Если текущие элементы равны
+                    dp[i][j] = dp[i-1][j-1] + 1  # Увеличиваем количество линий на 1
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])  # Иначе, выбираем максимум из двух предыдущих значений
 
-        return dp[m][n]
+        return dp[m][n]  # Возвращаем количество линий для последних элементов массивов nums1 и nums2
+    
+    
+
     
 list_nums1 = [2,5,1,2,5], [1,4,2],[1,3,7,1,7,5]
 list_nums2 = [10,5,2,1,5,2],[1,2,4],[1,9,2,5,1]
