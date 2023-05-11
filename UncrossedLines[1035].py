c3a1b0867599c94c58165a332138e198a712f164
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-11 23:19:43 +0200

    [2] final without explain

--- UncrossedLines[1035].py
+++ UncrossedLines[1035].py
@@ -43,7 +43,17 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        return True
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (n+1) for _ in range(m+1)] 
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if nums1[i-1] == nums2[j-1]: 
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[m][n]
     
 list_nums1 = [2,5,1,2,5], [1,4,2],[1,3,7,1,7,5]
 list_nums2 = [10,5,2,1,5,2],[1,2,4],[1,9,2,5,1]
