63cb318aee9b184f17494caa79f3be1b75cfbca5
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-29 05:44:47 +0200

    [1]final

--- DesignParkingSystem[1603].py
+++ DesignParkingSystem[1603].py
@@ -32,3 +32,31 @@
 # 186,428
 # Submissions
 # 211,828
+class ParkingSystem(object):
+
+    def __init__(self, big, medium, small):
+        """
+        :type big: int
+        :type medium: int
+        :type small: int
+        """
+        self.slots = [big, medium, small]
+        
+
+    def addCar(self, carType):
+        """
+        :type carType: int
+        :rtype: bool
+        """
+        if self.slots[carType - 1] > 0:
+            self.slots[carType - 1] -= 1
+            return True
+        else:
+            return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
+
+
