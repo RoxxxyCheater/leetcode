93eda2bbce9ecb36cecbdfddf38ef4a7c885955b
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-05-30 23:20:47 +0200

    [2] final without expl

--- FirstBadVersion[278].py
+++ FirstBadVersion[278].py
@@ -36,12 +36,21 @@
 # def isBadVersion(version):
 
 class Solution(object):
-    def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        return n
+        left = 1
+        right = n
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
     
 list_n = 5,1
 
