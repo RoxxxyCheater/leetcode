5005a31ccff905fb21e31f5641d4aa9442db7345
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-06-06 00:08:41 +0300

    [2] final

--- RobotReturnТoOrigin[657].py
+++ RobotReturnТoOrigin[657].py
@@ -39,7 +39,21 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        return moves
+        x = 0
+        y = 0
+
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'L':
+                x -= 1
+            elif move == 'R':
+                x += 1
+
+        return x == 0 and y == 0
+    
 list_move = "UP", "LL"
 for i in list_move:
    Solution.judgeCircle('Success', i)
