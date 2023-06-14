639a6231d101f352fc26258768906ff5b8641791
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-06-14 03:59:01 +0200

    [0] task

---
+++ MinimumAbsoluteDifferenceinBST[530].py
@@ -0,0 +1,38 @@
+# 530. MinimumAbsoluteDifferenceinBST
+# Easy
+
+# 2960
+
+# 155
+
+# Add to List
+
+# Share
+# Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [4,2,6,1,3]
+# Output: 1
+# Example 2:
+
+
+# Input: root = [1,0,48,null,null,12,49]
+# Output: 1
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [2, 104].
+# 0 <= Node.val <= 105
+ 
+
+# Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+# Accepted
+# 215,500
+# Submissions
+# 375,395
