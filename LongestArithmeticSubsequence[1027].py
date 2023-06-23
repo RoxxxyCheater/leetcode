7250162c299942e473614f314a55b95723729026
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-06-24 00:42:10 +0300

    [2] final without explain

--- LongestArithmeticSubsequence[1027].py
+++ LongestArithmeticSubsequence[1027].py
@@ -47,7 +47,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return res
+        n = len(nums)
+        dp = [{} for _ in range(n)]  
+        max_len = 2 
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j] 
+                if diff in dp[j]:
+                    dp[i][diff] = dp[j][diff] + 1
+                else:
+                    dp[i][diff] = 2
+                
+                max_len = max(max_len, dp[i][diff]) 
+        return max_len
 
 list_nums = [3,6,9,12],[9,4,7,2,10],[20,1,15,3,10,5,8]
 for i in list_nums:
