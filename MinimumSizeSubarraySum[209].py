3c202e870e9de0be602190b8c447a53dff58e6d9
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-06 23:44:17 +0200

    [2] final without explain

--- MinimumSizeSubarraySum[209].py
+++ MinimumSizeSubarraySum[209].py
@@ -48,7 +48,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return res
+        n = len(nums)
+        left = 0
+        subarray_sum = 0
+        min_length = float('inf')
+        
+        for right in range(n):
+            subarray_sum += nums[right]
+            
+            while subarray_sum >= target:
+                min_length = min(min_length, right - left + 1)
+                subarray_sum -= nums[left]
+                left += 1
+        
+        return min_length if min_length != float('inf') else 0
 
 list_t = 7, 4, 11
 list_n = [2,3,1,2,4,3], [1,4,4], [1,1,1,1,1,1,1,1]
