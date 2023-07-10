e94e53b02b7e1fb74ebc7050fab23667ea7510e1
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-10 22:58:50 +0200

    [1] func task

--- MissingNumber[268].py
+++ MissingNumber[268].py
@@ -1,45 +1,58 @@
-268. Missing Number
-Easy
+# 268. Missing Number
+# Easy
 
-9874
+# 9874
 
-3164
+# 3164
 
-Add to List
+# Add to List
 
-Share
-Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+# Share
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
  
 
-Example 1:
+# Example 1:
 
-Input: nums = [3,0,1]
-Output: 2
-Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
-Example 2:
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+# Example 2:
 
-Input: nums = [0,1]
-Output: 2
-Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
-Example 3:
+# Input: nums = [0,1]
+# Output: 2
+# Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+# Example 3:
 
-Input: nums = [9,6,4,2,3,5,7,0,1]
-Output: 8
-Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
  
 
-Constraints:
+# Constraints:
 
-n == nums.length
-1 <= n <= 104
-0 <= nums[i] <= n
-All the numbers of nums are unique.
+# n == nums.length
+# 1 <= n <= 104
+# 0 <= nums[i] <= n
+# All the numbers of nums are unique.
  
 
-Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+# Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
-Accepted
-1,564,007
-Submissions
-2,475,297
+# Accepted
+# 1,564,007
+# Submissions
+# 2,475,297
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return res
+
+list_n = [3,0,1], [0,1], [9,6,4,2,3,5,7,0,1]
+for i in list_n:
+  Solution.missingNumber('Success', i)
