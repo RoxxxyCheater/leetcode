5554f8b773631f9840b8d5d7fc1305980762f164
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-13 22:58:45 +0200

    [1] func task

--- DeleteNodeInALinkedList[237].py
+++ DeleteNodeInALinkedList[237].py
@@ -51,3 +51,22 @@
 # 1,100,204
 # Submissions
 # 1,435,002
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        return res
+
+list_h = [4,5,1,9],  [4,5,1,9]
+list_n = 5,1
+for i in list_n:
+  Solution.deleteNode('Success', i)
