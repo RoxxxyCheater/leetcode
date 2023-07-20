1fd34647b8ef9f0b9c366c974405624a20e64f93
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-20 23:36:38 +0300

    [2] final without explain

--- FizzBuzz[412].py
+++ FizzBuzz[412].py
@@ -42,7 +42,17 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        return res
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
 
 list_n = 3,5,15
 for i in list_n:
