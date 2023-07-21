e3985d169db806241d1b1bba3a7300e81f55652e
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-21 23:39:11 +0300

    ]

--- RangeSumQueryImmutable[303].py
+++ RangeSumQueryImmutable[303].py
@@ -39,3 +39,26 @@
 # 452,398
 # Submissions
 # 747,649
+
+
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        return res
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return res
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
