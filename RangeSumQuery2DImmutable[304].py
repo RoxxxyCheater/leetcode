1dd4cf0b3325e564ea0ead609d3150d4182b8cb2
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-07-24 23:44:26 +0300

    func task

--- RangeSumQuery2DImmutable[304].py
+++ RangeSumQuery2DImmutable[304].py
@@ -44,3 +44,30 @@
 # 328,585
 # Submissions
 # 617,598
+
+
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        return res
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return res
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+
