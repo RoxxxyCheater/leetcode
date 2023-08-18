7a7c69bdb2dd98a53ca2a3762246401c6e459653
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-08-18 23:47:27 +0300

    [2] final

--- LargestTriangleArea[812].py
+++ LargestTriangleArea[812].py
@@ -35,7 +35,19 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: float
         """
-        return res
+        def calculate_area(p1, p2, p3):
+            return 0.5 * abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]))
+    
+        max_area = 0
+        n = len(points)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    area = calculate_area(points[i], points[j], points[k])
+                    max_area = max(max_area, area)
+
+        return max_area
 
 list_p = [[0,0],[0,1],[1,0],[0,2],[2,0]], [[1,0],[0,0],[0,1]]
 for i in list_p:
