7a3a99a33d3f6b03336e14808eab4b87d089f148
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-08-20 22:30:19 +0200

    [2] final without explain

--- WordPattern[290].py
+++ WordPattern[290].py
@@ -49,7 +49,28 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        return res
+        words = s.split()
+        
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        word_to_char = {}
+        
+        for char, word in zip(pattern, words):
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                char_to_word[char] = word
+            
+            if word in word_to_char:
+                if word_to_char[word] != char:
+                    return False
+            else:
+                word_to_char[word] = char
+        
+        return True
 
 list_g =  "abba", "abba", "aaaa"
 list_s = "dog cat cat dog", "dog cat cat fish", "dog cat cat dog"
