9b0ed03d753e6ad404610f10bab38bb455a64ca6
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-08-27 22:11:32 +0300

    [2] final

--- FindFirstAndLastPositionOfElementinSortedArray[34].py
+++ FindFirstAndLastPositionOfElementinSortedArray[34].py
@@ -48,7 +48,38 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        return res
+        def findStart(nums, target):
+            left, right = 0, len(nums) - 1
+            start = -1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    start = mid
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return start
+        
+        def findEnd(nums, target):
+            left, right = 0, len(nums) - 1
+            end = -1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    end = mid
+                    left = mid + 1
+                elif nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return end
+        
+        start = findStart(nums, target)
+        end = findEnd(nums, target)
+        
+        return [start, end]
 
 
 
