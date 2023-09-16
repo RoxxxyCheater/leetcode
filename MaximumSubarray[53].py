246d8239425ac39a54ff818b0cc468d03114ea9e
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-09-16 22:42:23 +0300

    [2] final

--- MaximumSubarray[53].py
+++ MaximumSubarray[53].py
@@ -45,8 +45,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        return res
+        max_sum = nums[0]  
+        current_sum = nums[0] 
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+        return max_sum
 
 list_n = [-2,1,-3,4,-1,2,1,-5,4], [1], [5,4,-1,7,8]
 for i in list_n:
