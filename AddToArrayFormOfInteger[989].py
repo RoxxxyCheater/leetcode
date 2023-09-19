5872b80255ff0c78b336cc65c689c58ee32ef002
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-09-20 00:38:38 +0300

    [3] final with explaine

--- AddToArrayFormOfInteger[989].py
+++ AddToArrayFormOfInteger[989].py
@@ -5,8 +5,6 @@ import reduce
 
 # Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
 
- 
-
 # Example 1:
 
 # Input: num = [1,2,0,0], k = 34
@@ -26,6 +24,7 @@ import reduce
 # Explanation: 215 + 806 = 1021
 
 
+
 class Solution(object):
     def addToArrayForm(self, num, k):
         """
@@ -38,7 +37,8 @@ class Solution(object):
         # возвращаем результат в виде списка цифр числа, преобразовав его в строку и разделив на цифры с помощью map(int, ...)
         return [int(a) for a in str(res)]
 
-list_nums,list_keys = [1,2,0,0],[2,7,4],[2,1,5],34,181,806
+list_nums = [1,2,0,0], [2,7,4], [2,1,5]
+list_keys = 34, 181, 806
  
 for index, num in enumerate(list_nums):
     Solution.addToArrayForm('Success', num, list_keys[index])
