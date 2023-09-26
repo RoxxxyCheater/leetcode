3a22efd244a04c0c99b53f1ca3ea8632fe462b66
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-09-26 23:13:29 +0200

    [2] final

--- ReverseOnlyLetters[917].py
+++ ReverseOnlyLetters[917].py
@@ -47,7 +47,17 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return res
+        s = list(s)
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalpha():
+                left += 1
+            while left < right and not s[right].isalpha():
+                right -= 1
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return ''.join(s)
 
 list_s = "ab-cd", "a-bC-dEf-ghIj", "Test1ng-Leet=code-Q!"
 for i in list_s:
