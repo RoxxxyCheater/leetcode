4444203520db2d24f70cfd85aacbd25fa710160e
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-10-02 23:18:13 +0200

    [1] func task

--- ReverseStringII[541].py
+++ ReverseStringII[541].py
@@ -1,35 +1,49 @@
-541. Reverse String II
-Easy
+# 541. Reverse String II
+# Easy
 
-1790
+# 1790
 
-3506
+# 3506
 
-Add to List
+# Add to List
 
-Share
-Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+# Share
+# Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
 
-If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+# If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
 
  
 
-Example 1:
+# Example 1:
 
-Input: s = "abcdefg", k = 2
-Output: "bacdfeg"
-Example 2:
+# Input: s = "abcdefg", k = 2
+# Output: "bacdfeg"
+# Example 2:
 
-Input: s = "abcd", k = 2
-Output: "bacd"
+# Input: s = "abcd", k = 2
+# Output: "bacd"
  
 
-Constraints:
+# Constraints:
 
-1 <= s.length <= 104
-s consists of only lowercase English letters.
-1 <= k <= 104
-Accepted
-215,850
-Submissions
-428,319
+# 1 <= s.length <= 104
+# s consists of only lowercase English letters.
+# 1 <= k <= 104
+# Accepted
+# 215,850
+# Submissions
+# 428,319
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        return res
+
+list_s = "abcdefg", "abcd"
+list_k = 2, 2
+
+for index, s in enumerate(list_s):
+    Solution.reverseStr("Success", s, list_k[index])
