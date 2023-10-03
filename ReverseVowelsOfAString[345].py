9ff33407852ce14505bd2e430add9375817f3918
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-10-03 23:33:07 +0200

    [2] final

--- ReverseVowelsOfAString[345].py
+++ ReverseVowelsOfAString[345].py
@@ -38,8 +38,19 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """
-        return res
+        """       
+        s = list(s)
+        vowels = "aeiouAEIOU"
+        left, right = 0, len(s) - 1
+        while left < right
+            while left < right and s[left] not in vowels:
+                left += 1            
+            while left < right and s[right] not in vowels:
+                right -= 1
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return "".join(s)
 
 
 list_s = "hello", "leetcode"
