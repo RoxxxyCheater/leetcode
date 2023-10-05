b147b178b439b838c0a07ac4ea36f96b2d266cb9
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-10-05 23:06:31 +0200

    [2] final

--- CountSpecialQuadruplets[1995].py
+++ CountSpecialQuadruplets[1995].py
@@ -51,7 +51,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return res
+        count = 0
+        n = len(nums)
+
+        for a in range(n):
+            for b in range(a + 1, n):
+                for c in range(b + 1, n):
+                    for d in range(c + 1, n):
+                        if nums[a] + nums[b] + nums[c] == nums[d]:
+                            count += 1
+
+        return count
 
 
 list_n = [1,2,3,6], [3,3,6,4,5], [1,1,1,3,5]
