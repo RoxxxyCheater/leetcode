9f2672ae5cbe2a306f5f482d49b4da3e8d05e44c
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-11-04 10:13:55 +0200

    [2] final

--- FindKPairsWithSmallestSums[373].py
+++ FindKPairsWithSmallestSums[373].py
@@ -50,7 +50,21 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        return res
+        if not nums1 or not nums2:
+            return []    
+        heap = [] 
+        results = [] 
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(heap, (nums1[i] + nums2[j], i, j))    
+        push(0, 0) 
+        while heap and len(results) < k:
+            _, i, j = heapq.heappop(heap)  
+            results.append([nums1[i], nums2[j]]) 
+            push(i, j + 1)  
+            if j == 0:
+                push(i + 1, 0)     
+        return results
      
 list_num1 = [1,7,11], [1,1,2], [1,2]
 list_num2 = [2,4,6], [1,2,3], [3]
