bd06335b5ecab39af61916447e1d5a4ea391a66b
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-11-05 10:42:43 +0200

    [2] final

--- KthSmallestElementInASortedMatrix[378].py
+++ KthSmallestElementInASortedMatrix[378].py
@@ -49,10 +49,25 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        return res
+        n = len(matrix)
+        left, right = matrix[0][0], matrix[n - 1][n - 1]        
+        while left < right:
+            mid = left + (right - left) // 2
+            count = 0
+            j = n - 1            
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > mid:
+                    j -= 1
+                count += (j + 1)            
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid        
+        return left
 
 
 list_m = [[1,5,9],[10,11,13],[12,13,15]], [[-5]]
 list_k = 8, 1
 for index, m in enumerate(list_m):
    Solution.kthSmallest('Success', m, list_k[index])
+
