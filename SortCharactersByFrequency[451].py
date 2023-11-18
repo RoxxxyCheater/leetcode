c7eadcb7055ad5ec6583eff16377cc25e2766d78
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-11-18 14:45:30 +0200

    [2] final

--- SortCharactersByFrequency[451].py
+++ SortCharactersByFrequency[451].py
@@ -46,6 +46,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        char_freq = {}
+        for char in s:
+            char_freq[char] = char_freq.get(char, 0) + 1
+        sorted_chars = sorted(char_freq.keys(), key=lambda x: char_freq[x], reverse=True)
+        result = ''.join(char * char_freq[char] for char in sorted_chars)
         return result
 
 
