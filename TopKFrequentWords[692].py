5f0f00d2f4897850e7c6660c80890bf1ceb272c1
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-12-02 22:31:46 +0200

    [1] func task

--- TopKFrequentWords[692].py
+++ TopKFrequentWords[692].py
@@ -1,38 +1,53 @@
-. Top K Frequent Words
-Medium
+# 692 Top K Frequent Words
+# Medium
 
-Given an array of strings words and an integer k, return the k most frequent strings.
+# Given an array of strings words and an integer k, return the k most frequent strings.
 
-Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
-
- 
-
-Example 1:
-
-Input: words = ["i","love","leetcode","i","love","coding"], k = 2
-Output: ["i","love"]
-Explanation: "i" and "love" are the two most frequent words.
-Note that "i" comes before "love" due to a lower alphabetical order.
-
-Example 2:
-
-Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
-Output: ["the","is","sunny","day"]
-Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+# Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
 
  
 
-Constraints:
+# Example 1:
 
-    1 <= words.length <= 500
-    1 <= words[i].length <= 10
-    words[i] consists of lowercase English letters.
-    k is in the range [1, The number of unique words[i]]
+# Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+# Output: ["i","love"]
+# Explanation: "i" and "love" are the two most frequent words.
+# Note that "i" comes before "love" due to a lower alphabetical order.
+
+# Example 2:
+
+# Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+# Output: ["the","is","sunny","day"]
+# Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
 
  
 
-Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
-Accepted
-582,984
-Submissions
-1,011,876
+# Constraints:
+
+#     1 <= words.length <= 500
+#     1 <= words[i].length <= 10
+#     words[i] consists of lowercase English letters.
+#     k is in the range [1, The number of unique words[i]]
+
+ 
+
+# Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+# Accepted
+# 582,984
+# Submissions
+# 1,011,876
+
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        return res
+words = ["i","love","leetcode","i","love","coding"], ["the","day","is","sunny","the","the","the","sunny","is","is"]
+k = 2, 4
+
+for i, w in enumerate(words):
+  Solution.topKFrequent('Success', w, k[i])
