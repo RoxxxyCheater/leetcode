667ad91dfafa09737f9f3103e5d63e07186997b2
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-12-28 23:41:39 +0200

    [2] final

--- RemoveAllAdjacentDuplicatesInStringII[1209].py
+++ RemoveAllAdjacentDuplicatesInStringII[1209].py
@@ -50,7 +50,18 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        return res
+        stack = []
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1] = (char, stack[-1][1] + 1)
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append((char, 1))
+        result = ''
+        for char, count in stack:
+            result += char * count
+        return result
 
 l_s = "abcd", "deeedbbcccbdaa", "pbbcggttciiippooaais"
 l_k = 2, 3, 2
