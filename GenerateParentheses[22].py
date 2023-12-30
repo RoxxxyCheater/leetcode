459b91d63d90c69a8a7f70fdd30588912e123125
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-12-30 20:16:23 +0200

    Update GenerateParentheses[22].py

--- GenerateParentheses[22].py
+++ GenerateParentheses[22].py
@@ -25,3 +25,18 @@
 # 1,645,759
 # Submissions
 # 2,228,572
+
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return res
+
+l_n = 3, 1
+
+for n in l_n:
+   Solution.generateParenthesis('Success', n)
