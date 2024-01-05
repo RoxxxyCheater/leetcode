937673e7fb0010a80060e57758f8450d82494ea9
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-05 03:37:13 +0200

    [2] final

--- WordSearch[79].py
+++ WordSearch[79].py
@@ -48,7 +48,20 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        return res
+        def dfs(i, j, k):
+            if not (0 <= i < len(board) and 0 <= j < len(board[0])) or board[i][j] != word[k]:
+                return False            
+            if k == len(word) - 1:
+                return True
+            temp, board[i][j] = board[i][j], '/'
+            result = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            board[i][j] = temp            
+            return result  
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0):
+                    return True                
+        return False
 
 board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
 word = "ABCCED",  "SEE", "ABCB"
