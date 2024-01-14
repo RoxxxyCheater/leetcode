8f64638ded0a7f551ec2afc3eca0563bfd0385a2
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-14 22:47:35 +0100

    [2] final

--- PerfectNumber[507].py
+++ PerfectNumber[507].py
@@ -41,7 +41,15 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        return res
+        if num <= 1:
+            return False    
+        divisor_sum = 1 
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                divisor_sum += i
+                if i != num // i: 
+                    divisor_sum += num // i    
+        return divisor_sum == num
 
 l_n = 28,7
 
