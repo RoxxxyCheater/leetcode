3ad188e2d9227697c9523362fcefe46998e91742
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-14 22:47:05 +0100

    [1] func task

--- PerfectNumber[507].py
+++ PerfectNumber[507].py
@@ -1,35 +1,49 @@
-507. Perfect Number
-Easy
+# 507. Perfect Number
+# Easy
 
-1021
+# 1021
 
-1203
+# 1203
 
-Add to List
+# Add to List
 
-Share
-A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+# Share
+# A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
 
-Given an integer n, return true if n is a perfect number, otherwise return false.
+# Given an integer n, return true if n is a perfect number, otherwise return false.
 
  
 
-Example 1:
+# Example 1:
 
-Input: num = 28
-Output: true
-Explanation: 28 = 1 + 2 + 4 + 7 + 14
-1, 2, 4, 7, and 14 are all divisors of 28.
-Example 2:
+# Input: num = 28
+# Output: true
+# Explanation: 28 = 1 + 2 + 4 + 7 + 14
+# 1, 2, 4, 7, and 14 are all divisors of 28.
+# Example 2:
 
-Input: num = 7
-Output: false
+# Input: num = 7
+# Output: false
  
 
-Constraints:
+# Constraints:
 
-1 <= num <= 108
-Accepted
-167,584
-Submissions
-426,092
+# 1 <= num <= 108
+# Accepted
+# 167,584
+# Submissions
+# 426,092
+
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return res
+
+l_n = 28,7
+
+for n in l_n:
+   Solution.checkPerfectNumber('Success', n)
