773f41f2415167eff5522ea96098c4e96f29d028
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-15 02:59:19 +0100

    [1] func task

---
+++ CheckIfBinaryStringHasAtMostOneSegmentOfOnes[1784].py
@@ -0,0 +1,34 @@
+# 1784. Check if Binary String Has at Most One Segment of Ones
+# Easy
+
+# 315
+
+# 929
+
+# Add to List
+
+# Share
+# Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+ 
+
+# Example 1:
+
+# Input: s = "1001"
+# Output: false
+# Explanation: The ones do not form a contiguous segment.
+# Example 2:
+
+# Input: s = "110"
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 100
+# s[i]​​​​ is either '0' or '1'.
+# s[0] is '1'.
+# Accepted
+# 39,237
+# Submissions
+# 99,574
