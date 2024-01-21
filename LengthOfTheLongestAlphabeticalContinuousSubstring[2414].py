f22d0f1dcc384c618f5824225df160c8a36118d8
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-22 00:18:33 +0100

    [2] final with explain

--- LengthOfTheLongestAlphabeticalContinuousSubstring[2414].py
+++ LengthOfTheLongestAlphabeticalContinuousSubstring[2414].py
@@ -44,7 +44,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return res
+        if not s:
+            return 0
+        max_length = 1 
+        current_length = 1
+        for i in range(1, len(s)):
+            if ord(s[i]) == ord(s[i - 1]) + 1:
+                current_length += 1
+            else:
+                max_length = max(max_length, current_length)
+                current_length = 1
+        max_length = max(max_length, current_length)
+        return max_length
 
 l_s = "abacaba", "abcde"
 
