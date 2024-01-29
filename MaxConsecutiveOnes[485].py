7aaf75c02aa9cb09ef981d019d436b831a6096cc
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-29 22:24:56 +0100

    [1] func task

--- MaxConsecutiveOnes[485].py
+++ MaxConsecutiveOnes[485].py
@@ -31,3 +31,17 @@
 # 1,060,462
 # Submissions
 # 1,817,040
+
+
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return res
+
+
+l_n = [1,1,0,1,1,1], [1,0,1,1,0,1]
+for i in l_n:
+   Solution.findMaxConsecutiveOnes('Success', i)
