25d6a90669bbb8de72939e3d72b337e4dbc9a769
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-29 22:23:44 +0100

    [0] task

---
+++ MaxConsecutiveOnes[485].py
@@ -0,0 +1,33 @@
+# 485. Max Consecutive Ones
+# Easy
+
+# 5475
+
+# 451
+
+# Add to List
+
+# Share
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,0,1,1,1]
+# Output: 3
+# Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+# Example 2:
+
+# Input: nums = [1,0,1,1,0,1]
+# Output: 2
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
+# Accepted
+# 1,060,462
+# Submissions
+# 1,817,040
