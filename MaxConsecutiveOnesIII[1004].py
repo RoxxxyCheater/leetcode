89251497bc227ba84ae2bef0ff9e82867e22b20d
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-30 23:19:10 +0100

    [2] final

--- MaxConsecutiveOnesIII[1004].py
+++ MaxConsecutiveOnesIII[1004].py
@@ -44,7 +44,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        return res
+        left = 0
+        max_length = 0
+        zero_count = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+        return max_length
 
 
 list_n = [1,1,1,0,0,0,1,1,1,1,0], [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
