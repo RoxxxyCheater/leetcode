14dd7d9f2416cbe43616b9070ee90a19da71a5a3
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-01-31 05:53:32 +0100

    [2] final

--- LongestRepeatingCharacterReplacement[424].py
+++ LongestRepeatingCharacterReplacement[424].py
@@ -46,7 +46,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        return res
+        max_length = 0 
+        max_count = 0 
+        char_count = {}     
+        left = 0     
+        for right in range(len(s)):
+            char_count[s[right]] = char_count.get(s[right], 0) + 1      
+            max_count = max(max_count, char_count[s[right]]) 
+            if right - left + 1 - max_count > k:
+                char_count[s[left]] -= 1
+                left += 1    
+            max_length = max(max_length, right - left + 1)     
+        return max_length  
+
 
 ls = "ABAB", "AABABBA"
 lk = 2, 1
