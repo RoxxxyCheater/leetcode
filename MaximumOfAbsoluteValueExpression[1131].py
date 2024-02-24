f28f216eca753baa36de8165828c179d989747e6
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-02-24 19:32:50 +0200

    [3] final with explain

--- MaximumOfAbsoluteValueExpression[1131].py
+++ MaximumOfAbsoluteValueExpression[1131].py
@@ -39,17 +39,17 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: int
         """
-        max_val = 0
-        for sign1 in [-1, 1]:
-            for sign2 in [-1, 1]:
-                min_arr = float('inf')
-                max_arr = float('-inf')
-                for i in range(len(arr1)):
-                    cur_val = sign1 * arr1[i] + sign2 * arr2[i] + i
-                    min_arr = min(min_arr, cur_val)
-                    max_arr = max(max_arr, cur_val)
-                max_val = max(max_val, max_arr - min_arr)
-        return max_val
+        max_val = 0  # Инициализация переменной для хранения максимального значения выражения
+        for sign1 in [-1, 1]:  # Перебор всех возможных значений знаков для arr1[i]
+            for sign2 in [-1, 1]:  # Перебор всех возможных значений знаков для arr2[i]
+                min_arr = float('inf')  # Инициализация переменной для хранения минимального значения выражения
+                max_arr = float('-inf')  # Инициализация переменной для хранения максимального значения выражения
+                for i in range(len(arr1)):  # Перебор всех элементов массивов arr1 и arr2
+                    cur_val = sign1 * arr1[i] + sign2 * arr2[i] + i  # Вычисление текущего значения выражения
+                    min_arr = min(min_arr, cur_val)  # Обновление минимального значения
+                    max_arr = max(max_arr, cur_val)  # Обновление максимального значения
+                max_val = max(max_val, max_arr - min_arr)  # Обновление максимального значения выражения
+        return max_val  # Возврат максимального значения выражения
 
 arr = [1,2,3,4], [1,-2,-5,0,10]
 arr2 = [-1,4,5,6], [0,-2,-1,-7,-4]
