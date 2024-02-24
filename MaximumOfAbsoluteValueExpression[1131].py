8c2da1852e00ee2610611bc4d7b29719baa94eae
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-02-24 19:28:50 +0200

    [2] final

--- MaximumOfAbsoluteValueExpression[1131].py
+++ MaximumOfAbsoluteValueExpression[1131].py
@@ -39,7 +39,17 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: int
         """
-        return res
+        max_val = 0
+        for sign1 in [-1, 1]:
+            for sign2 in [-1, 1]:
+                min_arr = float('inf')
+                max_arr = float('-inf')
+                for i in range(len(arr1)):
+                    cur_val = sign1 * arr1[i] + sign2 * arr2[i] + i
+                    min_arr = min(min_arr, cur_val)
+                    max_arr = max(max_arr, cur_val)
+                max_val = max(max_val, max_arr - min_arr)
+        return max_val
 
 arr = [1,2,3,4], [1,-2,-5,0,10]
 arr2 = [-1,4,5,6], [0,-2,-1,-7,-4]
