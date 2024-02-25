a5cbf2438807aa8e391ab0e2de0ab500987b7ff1
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-02-25 05:52:08 +0200

    [1] func task

--- DivisorGame[1025].py
+++ DivisorGame[1025].py
@@ -1,34 +1,48 @@
-1025. Divisor Game
-Description
-Hints
-Submissions
-Discuss
-Solution
-Pick One
-Alice and Bob take turns playing a game, with Alice starting first.
+# 1025. Divisor Game
+# Description
+# Hints
+# Submissions
+# Discuss
+# Solution
+# Pick One
+# Alice and Bob take turns playing a game, with Alice starting first.
 
-Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+# Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
 
-Choosing any x with 0 < x < n and n % x == 0.
-Replacing the number n on the chalkboard with n - x.
-Also, if a player cannot make a move, they lose the game.
+# Choosing any x with 0 < x < n and n % x == 0.
+# Replacing the number n on the chalkboard with n - x.
+# Also, if a player cannot make a move, they lose the game.
 
-Return true if and only if Alice wins the game, assuming both players play optimally.
+# Return true if and only if Alice wins the game, assuming both players play optimally.
 
  
 
-Example 1:
+# Example 1:
 
-Input: n = 2
-Output: true
-Explanation: Alice chooses 1, and Bob has no more moves.
-Example 2:
+# Input: n = 2
+# Output: true
+# Explanation: Alice chooses 1, and Bob has no more moves.
+# Example 2:
 
-Input: n = 3
-Output: false
-Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+# Input: n = 3
+# Output: false
+# Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
  
 
-Constraints:
+# Constraints:
 
-1 <= n <= 1000
+# 1 <= n <= 1000
+
+
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return res
+
+ln = 2, 3
+
+for n in ln:
+   Solution.divisorGame('Success', n)
