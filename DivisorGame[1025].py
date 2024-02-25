a99d761893a6f08ffd6cee3e5c0072d28c557aa3
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-02-25 05:52:40 +0200

    [2] fina

--- DivisorGame[1025].py
+++ DivisorGame[1025].py
@@ -40,7 +40,14 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        return res
+        dp = [False] * (n + 1)
+        dp[1] = False
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                if i % j == 0 and not dp[i - j]:
+                    dp[i] = True
+                    break        
+        return dp[n]
 
 ln = 2, 3
 
