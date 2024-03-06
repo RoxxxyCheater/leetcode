56049bb5fd45bb8c91c9157d0197dc0de8e2c094
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-03-06 22:06:35 +0100

    [0] task

---
+++ Base7[504].py
@@ -0,0 +1,24 @@
+504. Base 7
+Description
+Hints
+Submissions
+Discuss
+Solution
+Pick One
+Given an integer num, return a string of its base 7 representation.
+
+ 
+
+Example 1:
+
+Input: num = 100
+Output: "202"
+Example 2:
+
+Input: num = -7
+Output: "-10"
+ 
+
+Constraints:
+
+-107 <= num <= 107
