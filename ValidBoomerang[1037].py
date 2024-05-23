b60ae88fcccdfa53f83fee361d0f72bf1b1dfdd8
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2024-05-23 22:16:14 +0200

    [0] task

---
+++ ValidBoomerang[1037].py
@@ -0,0 +1,28 @@
+1037. Valid Boomerang
+Description
+Hints
+Submissions
+Discuss
+Solution
+Pick One
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+ 
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+ 
+
+Constraints:
+
+points.length == 3
+points[i].length == 2
+0 <= xi, yi <= 100
